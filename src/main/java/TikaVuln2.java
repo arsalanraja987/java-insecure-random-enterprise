456bfd9f6a35330fa7de0f8a89e2e2f7fdcfbe73
arsalanraja987 <arsalanthepathanman@gmail.com>
2025-07-14 09:30:06 -0400

    Create TikaVuln2.java

---
+++ src/main/java/TikaVuln2.java
@@ -0,0 +1,8 @@
+import org.apache.tika.parser.AutoDetectParser;
+
+public class TikaVuln2 {
+    public void parseXml() throws Exception {
+        AutoDetectParser parser = new AutoDetectParser();
+        // No EntityResolver
+    }
+}
